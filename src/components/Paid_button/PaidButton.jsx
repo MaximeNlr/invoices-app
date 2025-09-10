9747a269930e0a6834016a0ad72f6a215057ee34
MaximeNlr <158460952+MaximeNlr@users.noreply.github.com>
2025-09-10 15:16:57 +0200

    add API to mark invoice as paid

--- src/components/Paid_button/PaidButton.jsx
+++ src/components/Paid_button/PaidButton.jsx
@@ -1,6 +1,22 @@
-export default function PaidButton() {
+export default function PaidButton({ id }) {
+
+    const paidButton = async () => {
+        try {
+            const options = {
+                method: 'PUT',
+                headers: { 'Content-type': 'application/json' },
+            }
+            const response = await fetch(`http://localhost:3000/api/invoice/${id}/paid`, options);
+            const data = await response.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <button
+            onClick={paidButton}
             className="bg-[var(--custom-color-1)] hover:bg-[var(--custom-color-2)]
              transition-colors rounded-3xl px-6 lg:px-8 py-2 text-white overflow-hidden whitespace-nowrap"
         >
